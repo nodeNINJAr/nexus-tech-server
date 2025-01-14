173dfbb3a2a21edbc9b37681901fd7a874402937
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-15 05:40:24 +0600

    user-info-save-to-db

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middleware
-app.use(cors());
+app.use(cors(
+  { 
+    origin:['http://localhost:5173']
+   }
+));
 app.use(express.json());
 app.use(morgan('dev'))
 
 
 
 
-
+// mongo db uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pm9ea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,11 +36,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 //   main code
+    // database collection
+     const database = client.db('nexusTech')
+     const usersCollection = database.collection('users')
 
 
 
-
-
+    // user info save to database
+    app.post('/users', async(req,res)=>{
+      const userInfo = req.body;
+      const query = {userEmail:userInfo?.userEmail}
+      const isExist = await usersCollection.findOne(query);
+      if(isExist) return res.status(409).send({message:"user info conflict"})
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result)
+    })
 
 
 
